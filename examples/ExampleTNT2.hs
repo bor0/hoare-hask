d83b09091e72441a0e7b2b90fa159fe293d9f95c
Boro Sitnikovski <buritomath@gmail.com>
2021-06-06 12:29:51 +0200

    Rewrite some of the examples with new neater syntax

--- examples/ExampleTNT2.hs
+++ examples/ExampleTNT2.hs
@@ -27,20 +27,17 @@ thm = do
 {- Prop1 -}
 -- |- 0=0+0
 prop1base = do
-  step1 <- axiom2 (Var A)
-  step2 <- ruleSpec Z step1
-  ruleSymmetry step2
+  step <- axiom2 (Var A) >>= ruleSpec Z
+  ruleSymmetry step
 
 -- |- All A:<A=0+A -> SA=0+SA>
 prop1hyp = do
   imp <- ruleFantasy (PropVar (Eq (Var A) (Plus Z (Var A)))) $ \premise -> do
     -- |- A=0+A -> SA=S(0+A)
     eq1 <- ruleAddS premise
-    step1 <- axiom3 (Var A) (Var B)
-    step2 <- ruleSpec Z step1
-    step3 <- ruleSpec (Var A) step2
+    step <- axiom3 (Var A) (Var B) >>= ruleSpec Z >>= ruleSpec (Var A)
     -- |- S(0+A)=0+SA
-    eq2 <- ruleSymmetry step3
+    eq2 <- ruleSymmetry step
     ruleTransitivity eq1 eq2
   ruleGeneralize A [] imp
 
@@ -51,30 +48,26 @@ prop1 = join $ ruleInduction <$> prop1base <*> prop1hyp
 -- |- SA+0=S(A+0)
 prop2base = do
   -- |- SA+0=SA
-  step1 <- axiom2 (Var A)
-  eq1 <- ruleSpec (S (Var A)) step1
+  eq1 <- axiom2 (Var A) >>= ruleSpec (S (Var A))
   -- |- A+0=A
-  eq2 <- ruleSpec (Var A) step1
+  eq2 <- axiom2 (Var A) >>= ruleSpec (Var A)
   -- |- S(A+0)=SA
-  eq2' <- ruleAddS eq2
+  eq2 <- ruleAddS eq2
   -- |- SA=S(A+0)
-  eq2'' <- ruleSymmetry eq2'
-  ruleTransitivity eq1 eq2''
+  eq2 <- ruleSymmetry eq2
+  ruleTransitivity eq1 eq2
 
 -- |- All A:<SA+B=S(A+B) -> SA+SB=S(A+SB)>
 prop2hyp = do
   imp <- ruleFantasy (PropVar (Eq (Plus (S (Var A)) (Var B)) (S (Plus (Var A) (Var B))))) $ \premise -> do
     -- |- SA+SB=S(SA+B)
-    step1 <- axiom3 (Var A) (Var B)
-    eq1 <- ruleSpec (S (Var A)) step1
-    eq1 <- ruleSpec (Var B) eq1
+    eq1 <- axiom3 (Var A) (Var B) >>= ruleSpec (S (Var A)) >>= ruleSpec (Var B)
     -- |- S(SA+B)=SS(A+B)
     eq2 <- ruleAddS premise
     -- |- SA+SB=SS(A+B)
     eq3 <- ruleTransitivity eq1 eq2
     -- |- A+SB=S(A+B)
-    eq4 <- ruleSpec (Var A) step1
-    eq4 <- ruleSpec (Var B) eq4
+    eq4 <- axiom3 (Var A) (Var B) >>= ruleSpec (Var A) >>= ruleSpec (Var B)
     -- |- S(A+SB)=SS(A+B)
     eq4' <- ruleAddS eq4
     -- |- SS(A+B)=S(A+SB)
@@ -88,12 +81,11 @@ prop2 = join (ruleInduction <$> prop2base <*> prop2hyp) >>= ruleGeneralize A []
 {- Prop3 -}
 -- |- B=C -> 0+B=0+C
 prop3base = ruleFantasy (PropVar (Eq (Var B) (Var C))) $ \premise -> do
-  prop1 <- prop1
-  step1 <- ruleSpec (Var B) prop1
+  step1 <- prop1 >>= ruleSpec (Var B)
   -- |- 0+B=B
   eq1 <- ruleSymmetry step1
   -- |- C=0+C
-  eq2 <- ruleSpec (Var C) prop1
+  eq2 <- prop1 >>= ruleSpec (Var C)
   -- |- 0+B=C
   eq3 <- ruleTransitivity eq1 premise
   ruleTransitivity eq3 eq2
@@ -102,15 +94,13 @@ prop3base = ruleFantasy (PropVar (Eq (Var B) (Var C))) $ \premise -> do
 prop3hyp = do
   imp <- ruleFantasy (Imp (PropVar (Eq (Var B) (Var C))) (PropVar (Eq (Plus (Var A) (Var B)) (Plus (Var A) (Var C))))) $ \premise -> do
    ruleFantasy (PropVar (Eq (Var B) (Var C))) $ \premise2 -> do
-     prop2 <- prop2
-     prop2 <- ruleSpec (Var A) prop2
      -- |- A+B=A+C
      eq1 <- ruleDetachment premise2 premise
      -- |- S(A+B)=S(A+C)
      eq2 <- ruleAddS eq1
      -- |- SA+B=S(A+B)
-     eq3 <- ruleSpec (Var B) prop2
-     eq3' <- ruleSpec (Var C) prop2
+     eq3 <- prop2 >>= ruleSpec (Var A) >>= ruleSpec (Var B)
+     eq3' <- prop2 >>= ruleSpec (Var A) >>= ruleSpec (Var C)
      -- |- S(A+C)=SA+C
      eq4 <- ruleSymmetry eq3'
      -- |- SA+B=S(A+C)
@@ -119,68 +109,45 @@ prop3hyp = do
   ruleGeneralize A [] imp
 
 -- |- All A:All B:All C:<B=C -> A+B=A+C>
-prop3 = do
-  eq1 <- join (ruleInduction <$> prop3base <*> prop3hyp)
-  -- |- B=C -> A+B=A+C
-  eq1 <- ruleSpec (Var A) eq1
-  -- |- All C:<B=C -> A+B=A+C>
-  eq2 <- ruleGeneralize C [] eq1
-  -- |- All B:All C:<B=C -> A+B=A+C>
-  eq3 <- ruleGeneralize B [] eq2
-  ruleGeneralize A [] eq3
+prop3 = join (ruleInduction <$> prop3base <*> prop3hyp) >>= ruleSpec (Var A) >>= ruleGeneralize C [] >>= ruleGeneralize B [] >>= ruleGeneralize A []
 
 {- Prop4 -}
 -- |- A+0=0+A
 prop4base = do
-  ax2 <- axiom2 (Var A)
-  prop1 <- prop1
-  eq1 <- ruleSpec (Var A) ax2
-  eq2 <- ruleSpec (Var A) prop1
+  eq1 <- axiom2 (Var A) >>= ruleSpec (Var A)
+  eq2 <- prop1 >>= ruleSpec (Var A)
   ruleTransitivity eq1 eq2
 
 -- |- All B:<A+B=B+A -> A+SB=SB+A>
 prop4hyp = do
   imp <- ruleFantasy (PropVar (Eq (Plus (Var A) (Var B)) (Plus (Var B) (Var A)))) $ \premise -> do
-    ax3 <- axiom3 (Var A) (Var B)
-    step1 <- ruleSpec (Var A) ax3
-    prop2 <- prop2
     -- |- A+SB=S(A+B)
-    eq1 <- ruleSpec (Var B) step1
+    eq1 <- axiom3 (Var A) (Var B) >>= ruleSpec (Var A) >>= ruleSpec (Var B)
     -- |- S(A+B)=S(B+A)
     eq2 <- ruleAddS premise
     -- |- A+SB=S(B+A)
     eq3 <- ruleTransitivity eq1 eq2
-    step2 <- ruleSpec (Var C) prop2
-    step2 <- ruleSpec (Var A) step2
-    step2 <- ruleGeneralize C [premise] step2
     -- |- SB+A=S(B+A)
-    eq4 <- ruleSpec (Var B) step2
+    eq4 <- prop2 >>= ruleSpec (Var C) >>= ruleSpec (Var A) >>= ruleGeneralize C [premise] >>= ruleSpec (Var B)
     -- |- S(B+A)=SB+A
     eq4' <- ruleSymmetry eq4
     ruleTransitivity eq3 eq4'
   ruleGeneralize B [] imp
 
 -- |- All A:All B:(A+B=B+A)
-prop4 = do
-  -- |- All B:(A+B=B+A)
-  eq1 <- join (ruleInduction <$> prop4base <*> prop4hyp)
-  ruleGeneralize A [] eq1
+prop4 = join (ruleInduction <$> prop4base <*> prop4hyp) >>= ruleGeneralize A []
 
 {- Prop5 -}
 -- |- A+B+0=(A+B)+0
 prop5base = do
-  ax2 <- axiom2 (Var A)
-  prop3 <- prop3
   -- |- B+0=B
-  eq1 <- ruleSpec (Var B) ax2
+  eq1 <- axiom2 (Var A) >>= ruleSpec (Var B)
   -- |- B+0=B -> A+B+0=A+B
-  eq2 <- ruleSpec (Var A) prop3
-  eq2 <- ruleSpec (Plus (Var B) Z) eq2
-  eq2 <- ruleSpec (Var B) eq2
+  eq2 <- prop3 >>= ruleSpec (Var A) >>= ruleSpec (Plus (Var B) Z) >>= ruleSpec (Var B)
   -- |- A+B+0=A+B
   eq3 <- ruleDetachment eq1 eq2
   -- |- A+B=(A+B)+0
-  eq4 <- ruleSpec (Plus (Var A) (Var B)) ax2
+  eq4 <- axiom2 (Var A) >>= ruleSpec (Plus (Var A) (Var B))
   eq4 <- ruleSymmetry eq4
   -- |- A+B+0=(A+B)+0
   eq5 <- ruleTransitivity eq3 eq4
@@ -189,26 +156,14 @@ prop5base = do
 -- |- All C:<A+B+C=(A+B)+C -> A+B+SC=(A+B)+SC>
 prop5hyp = do
   imp <- ruleFantasy (PropVar (Eq (Plus (Var A) (Plus (Var B) (Var C))) (Plus (Plus (Var A) (Var B)) (Var C)))) $ \premise -> do
-    ax3 <- axiom3 (Var A) (Var B)
-    prop3 <- prop3
     -- |- B+SC=S(B+C)
-    eq1 <- applyFOLRule [GoRight] (ruleSpec (Var C)) [] ax3
-    eq1 <- ruleSpec (Var B) eq1
-    -- |- B=S(D+C) -> A+B=A+S(D+C)
-    eq2 <- ruleSpec (Var A) prop3
-    eq2 <- ruleSpec (Var E) eq2
-    eq2 <- ruleSpec (S (Plus (Var D) (Var C))) eq2
-    -- |- B+SC=S(D+C) -> A+B+SC=A+S(D+C)
-    eq2' <- ruleGeneralize E [premise] eq2
-    eq2' <- ruleSpec (Plus (Var B) (S (Var C))) eq2'
+    eq1 <- axiom3 (Var A) (Var B) >>= applyFOLRule [GoRight] (ruleSpec (Var C)) [] >>= ruleSpec (Var B)
     -- |- B+SC=S(B+C) -> A+B+SC=A+S(B+C)
-    eq2'' <- ruleGeneralize D [premise] eq2'
-    eq2'' <- ruleSpec (Var B) eq2''
+    eq2 <- prop3 >>= ruleSpec (Var A) >>= ruleSpec (Var E) >>= ruleSpec (S (Plus (Var D) (Var C))) >>= ruleGeneralize E [premise] >>= ruleSpec (Plus (Var B) (S (Var C))) >>= ruleGeneralize D [premise] >>= ruleSpec (Var B)
     -- |- A+B+SC=A+S(B+C)
-    eq3 <- ruleDetachment eq1 eq2''
+    eq3 <- ruleDetachment eq1 eq2
     -- |- A+S(B+C)=S(A+B+C)
-    eq4 <- ruleSpec (Var A) ax3
-    eq4 <- ruleSpec (Plus (Var B) (Var C)) eq4
+    eq4 <- axiom3 (Var A) (Var B) >>= ruleSpec (Var A) >>= ruleSpec (Plus (Var B) (Var C))
     -- |- A+B+SC=S(A+B+C)
     eq5 <- ruleTransitivity eq3 eq4
     -- |- S(A+B+C)=S((A+B)+C)
@@ -216,39 +171,28 @@ prop5hyp = do
     -- |- A+B+SC=S((A+B)+C)
     eq7 <- ruleTransitivity eq5 eq6
     -- |- S((A+B)+C)=(A+B)+SC
-    eq8 <- applyFOLRule [GoRight] (ruleSpec (Var C)) [] ax3
-    eq8 <- ruleSpec (Plus (Var A) (Var B)) eq8
-    eq8 <- ruleSymmetry eq8
-    ruleTransitivity eq7 eq8
+    eq8 <- axiom3 (Var A) (Var B) >>= applyFOLRule [GoRight] (ruleSpec (Var C)) [] >>= ruleSpec (Plus (Var A) (Var B))
+    eq9 <- ruleSymmetry eq8
+    ruleTransitivity eq7 eq9
   ruleGeneralize C [] imp
 
 -- |- All A:All B:All C:(A+B+C=(A+B)+C)
-prop5 = do
-  -- |- All C:(A+B+C=(A+B)+C
-  eq1 <- join (ruleInduction <$> prop5base <*> prop5hyp)
-  -- |- All B:All C:(A+B+C=(A+B)+C)
-  eq2 <- ruleGeneralize B [] eq1
-  ruleGeneralize A [] eq2
+prop5 = join (ruleInduction <$> prop5base <*> prop5hyp) >>= ruleGeneralize B [] >>= ruleGeneralize A []
 
 {- Prop6 -}
 -- |- 0*0=0
-prop6base = do
-  ax4 <- axiom4 (Var A)
-  ruleSpec Z ax4
+prop6base = axiom4 (Var A) >>= ruleSpec Z
 
 -- |- All A:<0*A=0 -> 0*SA=0>
 prop6hyp = do
   imp <- ruleFantasy (PropVar (Eq (Mult Z (Var A)) Z)) $ \premise -> do
-    ax5 <- axiom5 (Var A) (Var B)
-    ax2 <- axiom2 (Var A)
-    eq1 <- ruleSpec Z ax5
     -- |- 0*SA=0*A+0
-    eq2 <- ruleSpec (Var A) eq1
+    eq1 <- axiom5 (Var A) (Var B) >>= ruleSpec Z >>= ruleSpec (Var A)
     -- |- 0*A+0=0*A
-    eq3 <- ruleSpec (Mult Z (Var A)) ax2
+    eq2 <- axiom2 (Var A) >>= ruleSpec (Mult Z (Var A))
     -- |- 0*SA=0*A
-    eq4 <- ruleTransitivity eq2 eq3
-    ruleTransitivity eq4 premise
+    eq3 <- ruleTransitivity eq1 eq2
+    ruleTransitivity eq3 premise
   ruleGeneralize A [] imp
 
 -- |- All A:(0*A=0)
@@ -257,22 +201,16 @@ prop6 = join (ruleInduction <$> prop6base <*> prop6hyp)
 {- Prop7 -}
 -- |- SA*0=A*0+0
 prop7base = do
-  ax4 <- axiom4 (Var A)
-  ax2 <- axiom2 (Var A)
-  prop3 <- prop3
-  prop4 <- prop4
   -- |- SA*0=0
-  eq1 <- ruleSpec (S (Var A)) ax4
+  eq1 <- axiom4 (Var A) >>= ruleSpec (S (Var A))
   -- |- 0=0+0
-  eq2 <- ruleSpec Z ax2
+  eq2 <- axiom2 (Var A) >>= ruleSpec Z
   eq2 <- ruleSymmetry eq2
   -- |- SA*0=0+0
   eq2' <- ruleTransitivity eq1 eq2
   -- |- A*0=0
-  eq3 <- ruleSpec (Var A) ax4
-  eq4 <- ruleSpec Z prop3
-  eq4 <- ruleSpec (Mult (Var A) Z) eq4
-  eq4 <- ruleSpec Z eq4
+  eq3 <- axiom4 (Var A) >>= ruleSpec (Var A)
+  eq4 <- prop3 >>= ruleSpec Z >>= ruleSpec (Mult (Var A) Z) >>= ruleSpec Z
   -- |- 0+0=0+A*0
   eq4' <- ruleDetachment eq3 eq4
   eq4' <- ruleSymmetry eq4'
@@ -281,131 +219,79 @@ prop7base = do
   -- |- SA*0=0+A*0
   eq6 <- ruleTransitivity eq5 eq4'
   -- |- 0+A*0=A*0+0
-  eq7 <- ruleSpec Z prop4
-  eq7 <- ruleSpec (Mult (Var A) Z) eq7
+  eq7 <- prop4 >>= ruleSpec Z >>= ruleSpec (Mult (Var A) Z)
   ruleTransitivity eq6 eq7
 
 -- |- All B:<SA*B=A*B+B -> SA*SB=A*SB+SB>
 prop7hyp = do
   imp <- ruleFantasy (PropVar (Eq (Mult (S (Var A)) (Var B)) (Plus (Mult (Var A) (Var B)) (Var B)))) $ \premise -> do
-    ax3 <- axiom3 (Var A) (Var B)
-    ax5 <- axiom5 (Var A) (Var B)
-    prop3 <- prop3
-    prop4 <- prop4
-    prop5 <- prop5
     -- |- SA*SB=SA*B+SA
-    eq1 <- ruleSpec (S (Var A)) ax5
-    eq1 <- ruleSpec (Var B) eq1
+    eq1 <- axiom5 (Var A) (Var B) >>= ruleSpec (S (Var A)) >>= ruleSpec (Var B)
     -- |- SA*B+SA=SA+SA*B
-    eq2 <- ruleSpec (Var D) prop4
-    eq2 <- ruleSpec (S (Var A)) eq2
-    eq2 <- ruleGeneralize D [premise] eq2
-    eq2 <- ruleSpec (Mult (S (Var A)) (Var B)) eq2
+    eq2 <- prop4 >>= ruleSpec (Var D) >>= ruleSpec (S (Var A)) >>= ruleGeneralize D [premise] >>= ruleSpec (Mult (S (Var A)) (Var B))
     eq2' <- ruleTransitivity eq1 eq2
-    eq3 <- ruleSpec (S (Var A)) prop3
-    eq3 <- ruleSpec (Mult (S (Var A)) (Var B)) eq3
-    eq3 <- ruleSpec (Plus (Mult (Var A) (Var B)) (Var B)) eq3
+    eq3 <- prop3 >>= ruleSpec (S (Var A)) >>= ruleSpec (Mult (S (Var A)) (Var B)) >>= ruleSpec (Plus (Mult (Var A) (Var B)) (Var B))
     eq3' <- ruleDetachment premise eq3
     -- |- SA*SB=SA+A*B+B
     eq4 <- ruleTransitivity eq2' eq3'
     -- |- SA+A*B+B=(A*B+B)+SA
-    eq5 <- ruleSpec (Var D) prop4
-    eq5 <- ruleSpec (Plus (Mult (Var A) (Var B)) (Var B)) eq5
-    eq5 <- ruleGeneralize D [premise] eq5
+    eq5 <- prop4 >>= ruleSpec (Var D) >>= ruleSpec (Plus (Mult (Var A) (Var B)) (Var B)) >>= ruleGeneralize D [premise]
     eq5 <- ruleSpec (S (Var A)) eq5
     -- |- SA*SB=(A*B+B)+SA
     eq6 <- ruleTransitivity eq4 eq5
     -- |- A*B+B+SA=(A*B+B)+SA
-    eq7 <- applyFOLRule [GoRight] (ruleSpec (Var B)) [] prop5
-    eq7 <- ruleSpec (Mult (Var A) (Var B)) eq7
-    eq7 <- ruleSpec (S (Var A)) eq7
+    eq7 <- prop5 >>= applyFOLRule [GoRight] (ruleSpec (Var B)) [] >>= ruleSpec (Mult (Var A) (Var B))>>= ruleSpec (S (Var A))
     eq7 <- ruleSymmetry eq7
     -- |- SA*SB=A*B+B+SA
     eq8 <- ruleTransitivity eq6 eq7
     -- |- B+SA=S(B+A)
-    eq9 <- ruleSpec (Var D) ax3
-    eq9 <- ruleSpec (Var A) eq9
-    eq9 <- ruleGeneralize D [premise] eq9
-    eq9 <- ruleSpec (Var B) eq9
+    eq9 <- axiom3 (Var A) (Var B) >>= ruleSpec (Var D) >>= ruleSpec (Var A) >>= ruleGeneralize D [premise] >>= ruleSpec (Var B)
     -- |- B+SA=S(B+A) -> SA+B+SA=SA+S(B+A)
-    eq9' <- ruleSpec (Var D) prop3
-    eq9' <- ruleSpec (Plus (Var B) (S (Var A))) eq9'
-    eq9' <- ruleSpec (S (Plus (Var B) (Var A))) eq9'
-    eq9' <- ruleGeneralize D [premise] eq9'
-    eq9' <- ruleSpec (Mult (Var A) (Var B)) eq9'
+    eq9' <- prop3 >>= ruleSpec (Var D) >>= ruleSpec (Plus (Var B) (S (Var A))) >>= ruleSpec (S (Plus (Var B) (Var A))) >>= ruleGeneralize D [premise] >>= ruleSpec (Mult (Var A) (Var B))
     -- |- A*B+B+SA=A*B+S(B+A)
     eq9'' <- ruleDetachment eq9 eq9'
     -- |- SA*SB=A*B+S(B+A)
     eq10 <- ruleTransitivity eq8 eq9''
     -- |- S(B+A)=S(A+B)
-    eq11 <- ruleSpec (Var D) prop4
-    eq11 <- ruleSpec (Var A) eq11
-    eq11 <- ruleGeneralize D [premise] eq11
-    eq11 <- ruleSpec (Var B) eq11
+    eq11 <- prop4 >>= ruleSpec (Var D) >>= ruleSpec (Var A) >>= ruleGeneralize D [premise]  >>= ruleSpec (Var B)
     eq11 <- ruleAddS eq11
     -- |- S(B+A)=S(A+B) -> A*B+S(B+A)=A*B+S(A+B)
-    eq11' <- ruleSpec (Var D) prop3
-    eq11' <- ruleSpec (S (Plus (Var B) (Var A))) eq11'
-    eq11' <- ruleSpec (S (Plus (Var A) (Var B))) eq11'
-    eq11' <- ruleGeneralize D [premise] eq11'
-    eq11' <- ruleSpec (Mult (Var A) (Var B)) eq11'
+    eq11' <- prop3 >>= ruleSpec (Var D) >>= ruleSpec (S (Plus (Var B) (Var A))) >>= ruleSpec (S (Plus (Var A) (Var B))) >>= ruleGeneralize D [premise] >>= ruleSpec (Mult (Var A) (Var B))
     -- |- A*B+S(B+A)=A*B+S(A+B)
     eq11'' <- ruleDetachment eq11 eq11'
     -- |- SA*SB=A*B+S(A+B)
     eq12 <- ruleTransitivity eq10 eq11''
     -- |- A+SB=S(A+B)
-    eq13 <- ruleSpec (Var A) ax3
-    eq13 <- ruleSpec (Var B) eq13
+    eq13 <- axiom3 (Var A) (Var B) >>= ruleSpec (Var A) >>= ruleSpec (Var B)
     -- |- S(B+A)=S(A+B) -> A*B+S(B+A)=A*B+S(A+B)
-    eq13' <- ruleSpec (Var D) prop3
-    eq13' <- ruleSpec (Plus (Var A) (S (Var B))) eq13'
-    eq13' <- ruleSpec (S (Plus (Var A) (Var B))) eq13'
-    eq13' <- ruleGeneralize D [premise] eq13'
-    eq13' <- ruleSpec (Mult (Var A) (Var B)) eq13'
+    eq13' <- prop3 >>= ruleSpec (Var D) >>= ruleSpec (Plus (Var A) (S (Var B))) >>= ruleSpec (S (Plus (Var A) (Var B))) >>= ruleGeneralize D [premise] >>= ruleSpec (Mult (Var A) (Var B))
     -- |- A*B+S(A+B)=A*B+A+SB
     eq13'' <- ruleDetachment eq13 eq13'
     eq13'' <- ruleSymmetry eq13''
     eq14 <- ruleTransitivity eq12 eq13''
     -- |- A*B+A+SB=(A*B+A)+SB
-    eq15 <- ruleSpec (Var D) prop5
-    eq15 <- ruleSpec (Var A) eq15
-    eq15 <- ruleSpec (S (Var B)) eq15
-    eq15 <- ruleGeneralize D [premise] eq15
-    eq15 <- ruleSpec (Mult (Var A) (Var B)) eq15
+    eq15 <- prop5 >>= ruleSpec (Var D) >>= ruleSpec (Var A) >>= ruleSpec (S (Var B)) >>= ruleGeneralize D [premise] >>= ruleSpec (Mult (Var A) (Var B))
     -- |- SA*SB=(A*B+A)+SB
     eq16 <- ruleTransitivity eq14 eq15
     -- |- A*SB=A*B+A
-    eq17 <- ruleSpec (Var A) ax5
-    eq17 <- ruleSpec (Var B) eq17
+    eq17 <- axiom5 (Var A) (Var B) >>= ruleSpec (Var A) >>= ruleSpec (Var B)
     -- |- A*SB=A*B+A -> SB+A*SB=SB+A*B+A
-    eq17' <- ruleSpec (Var D) prop3
-    eq17' <- ruleSpec (Mult (Var A) (S (Var B))) eq17'
-    eq17' <- ruleSpec (Plus (Mult (Var A) (Var B)) (Var A)) eq17'
-    eq17' <- ruleGeneralize D [premise] eq17'
-    eq17' <- ruleSpec (S (Var B)) eq17'
+    eq17' <- prop3 >>= ruleSpec (Var D) >>= ruleSpec (Mult (Var A) (S (Var B))) >>= ruleSpec (Plus (Mult (Var A) (Var B)) (Var A)) >>= ruleGeneralize D [premise] >>= ruleSpec (S (Var B))
     -- |- SB+A*SB=SB+A*B+A
     eq17'' <- ruleDetachment eq17 eq17'
     -- |- SB+A*B+A=(A*B+A)+SB
-    eq17''' <- ruleSpec (Var D) prop4
-    eq17''' <- ruleSpec (Plus (Mult (Var A) (Var B)) (Var A)) eq17'''
-    eq17''' <- ruleGeneralize D [premise] eq17'''
-    eq17''' <- ruleSpec (S (Var B)) eq17'''
+    eq17''' <- prop4 >>= ruleSpec (Var D) >>= ruleSpec (Plus (Mult (Var A) (Var B)) (Var A)) >>= ruleGeneralize D [premise] >>= ruleSpec (S (Var B))
     eq17'''' <- ruleTransitivity eq17'' eq17'''
     eq17'''' <- ruleSymmetry eq17''''
     -- |- SA*SB=SB+A*SB
     eq18 <- ruleTransitivity eq16 eq17''''
     -- |- SB+A*SB=A*SB+SB
-    eq19 <- ruleSpec (Var D) prop4
-    eq19 <- ruleSpec (Mult (Var A) (S (Var B))) eq19
-    eq19 <- ruleGeneralize D [premise] eq19
-    eq19 <- ruleSpec (S (Var B)) eq19
+    eq19 <- prop4 >>= ruleSpec (Var D) >>= ruleSpec (Mult (Var A) (S (Var B))) >>= ruleGeneralize D [premise] >>= ruleSpec (S (Var B))
     ruleTransitivity eq18 eq19
   ruleGeneralize B [] imp
 
 -- |- All A:All B:(SA*B=A*B+B)
-prop7 = do
-  eq1 <- join (ruleInduction <$> prop7base <*> prop7hyp)
-  ruleGeneralize A [] eq1
+prop7 = join (ruleInduction <$> prop7base <*> prop7hyp) >>=  ruleGeneralize A []
 
 {- Prop8 -}
 -- |- A*0=0*A
@@ -420,41 +306,25 @@ prop8base = do
 -- |- All B:<A*B=B*A -> A*SB=SB*A>
 prop8hyp = do
   imp <- ruleFantasy (PropVar (Eq (Mult (Var A) (Var B)) (Mult (Var B) (Var A)))) $ \premise -> do
-    ax5 <- axiom5 (Var A) (Var B)
-    prop3 <- prop3
-    prop4 <- prop4
-    prop7 <- prop7
     -- |- A*SB=A*B+A
-    eq1 <- ruleSpec (Var A) ax5
-    eq1 <- ruleSpec (Var B) eq1
+    eq1 <- axiom5 (Var A) (Var B) >>= ruleSpec (Var A) >>= ruleSpec (Var B)
     -- |- A*B=B*A -> A+A*B=A+B*A
-    eq2 <- ruleSpec (Var A) prop3
-    eq2 <- ruleSpec (Mult (Var A) (Var B)) eq2
-    eq2 <- ruleSpec (Mult (Var B) (Var A)) eq2
+    eq2 <- prop3 >>= ruleSpec (Var A) >>= ruleSpec (Mult (Var A) (Var B)) >>= ruleSpec (Mult (Var B) (Var A))
     -- |- A+A*B=A+B*A
     eq2' <- ruleDetachment premise eq2
     -- |- A*B+A=A+A*B
-    eq3 <- ruleSpec (Var D) prop4
-    eq3 <- ruleSpec (Var A) eq3
-    eq3 <- ruleGeneralize D [premise] eq3
-    eq3 <- ruleSpec (Mult (Var A) (Var B)) eq3
+    eq3 <- prop4 >>= ruleSpec (Var D) >>= ruleSpec (Var A) >>= ruleGeneralize D [premise] >>= ruleSpec (Mult (Var A) (Var B))
     -- |- A*SB=A+A*B
     eq4 <- ruleTransitivity eq1 eq3
     -- |- A*SB=A+B*A
     eq5 <- ruleTransitivity eq4 eq2'
     -- |- A+B*A=B*A+A
-    eq6 <- ruleSpec (Var D) prop4
-    eq6 <- ruleSpec (Var A) eq6
-    eq6 <- ruleGeneralize D [premise] eq6
-    eq6 <- ruleSpec (Mult (Var B) (Var A)) eq6
+    eq6 <- prop4 >>= ruleSpec (Var D) >>= ruleSpec (Var A) >>= ruleGeneralize D [premise] >>= ruleSpec (Mult (Var B) (Var A))
     eq6 <- ruleSymmetry eq6
     -- |- A*SB=B*A+A
     eq7 <- ruleTransitivity eq5 eq6
     -- |- B*A+A=SB*A
-    eq8 <- ruleSpec (Var D) prop7
-    eq8 <- ruleSpec (Var A) eq8
-    eq8 <- ruleGeneralize D [premise] eq8
-    eq8 <- ruleSpec (Var B) eq8
+    eq8 <- prop7 >>= ruleSpec (Var D) >>= ruleSpec (Var A) >>= ruleGeneralize D [premise] >>= ruleSpec (Var B)
     eq8 <- ruleSymmetry eq8
     ruleTransitivity eq7 eq8
   ruleGeneralize B [] imp
